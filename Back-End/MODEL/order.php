40b6b45863ad4d54ca06edd16041bb7e3bfdd16c
TemporinSimone <90798726+TemporinSimone@users.noreply.github.com>
2023-06-08 10:47:39 +0200

    Update order.php

--- Back-End/MODEL/order.php
+++ Back-End/MODEL/order.php
@@ -1,10 +1,12 @@
 <?php
-
+// Registra la funzione di caricamento automatico delle classi
 spl_autoload_register(function ($class) {
     require __DIR__ . "/../COMMON/$class.php";
 });
 
+// Imposta la gestione delle eccezioni personalizzata
 set_exception_handler("errorHandler::handleException");
+// Imposta la gestione degli errori personalizzata
 set_error_handler("errorHandler::handleError");
 
 class Order
@@ -14,15 +16,17 @@ class Order
 
     public function __construct()
     {
+        // Crea un'istanza di Connect per stabilire la connessione al database
         $this->db = new Connect;
         $this->conn = $this->db->getConnection();
     }
     
-        public function getArchiveOrder() //Ritorna tutti gli ordini.
+    // Ottiene tutti gli ordini dall'archivio
+    public function getArchiveOrder()
     {
         $query = "SELECT o.id, a.username, o.address, o.date_order, o.total_price, o.status
-        from `order` o
-        inner join account a on o.id_account = a.id";
+        FROM `order` o
+        INNER JOIN account a ON o.id_account = a.id";
 
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
@@ -30,12 +34,13 @@ class Order
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function getOrder($id) //ritorna l'ordine richiesto ricevendo in input l'id dell'ordine stesso
+    // Ottiene un ordine specifico
+    public function getOrder($id)
     {
         $sql = "SELECT o.id, a.username , o.address, o.date_order, o.total_price, o.status
-        from `order` o
-        inner join account a on a.id = o.id_account 
-        where o.id = :id";
+        FROM `order` o
+        INNER JOIN account a ON a.id = o.id_account 
+        WHERE o.id = :id";
 
         $stmt = $this->conn->prepare($sql);
         $stmt->bindValue(':id', $id, PDO::PARAM_INT);
@@ -44,13 +49,14 @@ class Order
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
     
-    public function getOrderFormaggy($id) //Ritorna i prodotti di un ordine
+    // Ottiene i prodotti di un ordine specifico
+    public function getOrderFormaggy($id)
     {
         $sql = "SELECT f.id, f.name, f.description, of2.weight
-        from `order` o
-        inner join order_formaggyo of2 on of2.id_order = o.id
-        inner join formaggyo f on f.id = of2.id_formaggyo 
-        where o.id = :id";
+        FROM `order` o
+        INNER JOIN order_formaggyo of2 ON of2.id_order = o.id
+        INNER JOIN formaggyo f ON f.id = of2.id_formaggyo 
+        WHERE o.id = :id";
 
         $stmt = $this->conn->prepare($sql);
         $stmt->bindValue(':id', $id, PDO::PARAM_INT);
@@ -58,30 +64,34 @@ class Order
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-        public function modifyOrderStatus($status,$id_order)
+    
+    // Modifica lo stato di un ordine
+    public function modifyOrderStatus($status, $id_order)
     {
-        $sql="UPDATE `order`
+        $sql = "UPDATE `order`
              SET status = :status
-             WHERE id=:id_order";
+             WHERE id = :id_order";
 
         $stmt = $this->conn->prepare($sql);
-        $stmt->bindValue(':status', $status , PDO::PARAM_INT);
+        $stmt->bindValue(':status', $status, PDO::PARAM_INT);
         $stmt->bindValue(':id_order', $id_order, PDO::PARAM_INT);
         $stmt->execute();
+
         return $stmt->rowCount();
     }
 
+    // Elimina un ordine
     public function deleteOrder($id_order)
     {
-        $sql="UPDATE `order`
-            set status = 3
-            where id=:id_order";
+        $sql = "UPDATE `order`
+            SET status = 3
+            WHERE id = :id_order";
 
         $stmt = $this->conn->prepare($sql);
         $stmt->bindValue(':id_order', $id_order, PDO::PARAM_INT);
         $stmt->execute();
-        return $stmt->rowCount();
 
+        return $stmt->rowCount();
     }
 }
 ?>
