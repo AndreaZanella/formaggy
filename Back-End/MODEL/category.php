1dd6144b9369aaac6c21153409423323c70e9961
TemporinSimone <90798726+TemporinSimone@users.noreply.github.com>
2023-06-08 10:04:37 +0200

    Update category.php

--- Back-End/MODEL/category.php
+++ Back-End/MODEL/category.php
@@ -8,24 +8,24 @@ set_error_handler("errorHandler::handleError");
 
 class Category
 {
-    private Connect $db;
-    private PDO $conn;
+    private Connect $db; // Oggetto Connect per la connessione al database
+    private PDO $conn; // Oggetto PDO per l'esecuzione delle query
 
-    public function __construct() //Si connette al DB.
+    public function __construct() // Costruttore della classe Category
     {
-        $this->db = new Connect;
-        $this->conn = $this->db->getConnection();
+        $this->db = new Connect; // Creazione di un'istanza dell'oggetto Connect
+        $this->conn = $this->db->getConnection(); // Connessione al database attraverso il metodo getConnection()
     }
 
-    public function getArchiveCategory() //Ritorna tutte le categorie.
+    public function getArchiveCategory() // Metodo per ottenere tutte le categorie dall'archivio
     {
         $query = "SELECT id, name
-        from category c";
+        from category c"; // Query per selezionare l'id e il nome delle categorie dalla tabella "category"
 
-        $stmt = $this->conn->prepare($query);
-        $stmt->execute();
+        $stmt = $this->conn->prepare($query); // Preparazione della query
+        $stmt->execute(); // Esecuzione della query
 
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC); // Restituzione dei risultati della query come array associativo
     }
 }
 ?>
